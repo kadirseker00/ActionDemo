6b498f4d8561eb84f51b3ed13c829255a1d233c8
kadirseker <kadirseker00@gmail.com>
2023-11-15 22:33:38 +0300

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,2 +1,18 @@
-def add_new(x,y):
-    return x+y
+import unittest
+
+class TestArithmetic(unittest.TestCase):
+
+    def test_add(self):
+        self.assertEqual(add(1, 2), 3)
+
+    def test_subtract(self):
+        self.assertEqual(subtract(10, 5), 5)
+
+    def test_multiply(self):
+        self.assertEqual(multiply(2, 3), 6)
+
+    def test_divide(self):
+        self.assertEqual(divide(10, 2), 5)
+
+if __name__ == "__main__":
+    unittest.main()
